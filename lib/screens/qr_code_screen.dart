a0693ebf2a78b4b10996a3631157f659c94aef6f
Matthieu <matthieunadeau96@gmail.com>
2021-11-19 10:42:57 -0500

    add QrCode to QrCodeScreen
    Use Futurebuilder to display qrcode using the ApiService's getSeed method

--- lib/screens/qr_code_screen.dart
+++ lib/screens/qr_code_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:superformula_qr_code/models/seed_model.dart';
+import 'package:superformula_qr_code/services/api_service.dart';
 
 class QrCodeScreen extends StatelessWidget {
   const QrCodeScreen({Key? key}) : super(key: key);
@@ -10,8 +13,35 @@ class QrCodeScreen extends StatelessWidget {
         title: const Text('QR Code'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('QR Code'),
+      body: FutureBuilder<SeedModel>(
+        future: ApiService().getSeed(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasData) {
+            var seed = snapshot.data!.seed;
+            print('seed -> $seed');
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: QrImage(
+                    data: seed,
+                    size: 250,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Text('15 s')
+              ],
+            );
+          }
+          return const Center(
+            child: Text('Something went wrong'),
+          );
+        },
       ),
     );
   }

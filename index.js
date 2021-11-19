9f367cfd7c7c308c38338402483a953d4dc9772c
Matthieu <matthieunadeau96@gmail.com>
2021-11-19 10:50:05 -0500

    fixed api to generate seed on each call

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ app.listen(
 );
 
 // Generates random seed
-let seedGen = Math.random().toString(36).substr(2, 3) + Math.random().toString(36).substr(2, 3) + Math.random().toString(36).substr(2, 3) + Math.random().toString(36).substr(2, 3) + Math.random(36).toString().substr(2, 4);
 
 app.get('/seed', (req, res) => {
+    let seedGen = Math.random().toString(36).substr(2, 3) + Math.random().toString(36).substr(2, 3) + Math.random().toString(36).substr(2, 3) + Math.random().toString(36).substr(2, 3) + Math.random(36).toString().substr(2, 4);
     res.status(200).send({
         seed: seedGen,
         description: 'seed generated'
     })
 });
+
+// app.get('/seed', (req, res) => {
+//     res.status(200).send({
+//         seed: seedGen,
+//         description: 'seed generated'
+//     })
+// });
+

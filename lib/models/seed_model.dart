6df614c2527c159170d1380517754e0ec4a457dd
Matthieu <matthieunadeau96@gmail.com>
2021-11-20 10:42:25 -0500

    Add expiration date to SeedModel

--- lib/models/seed_model.dart
+++ lib/models/seed_model.dart
@@ -1,16 +1,21 @@
+import 'package:flutter/material.dart';
+
 class SeedModel {
   final String seed;
   final String description;
+  final String expirationDate;
 
   SeedModel({
     required this.seed,
     required this.description,
+    required this.expirationDate,
   });
 
   factory SeedModel.fromJson(Map<String, dynamic> json) {
     return SeedModel(
       seed: json['seed'],
       description: json['description'],
+      expirationDate: json['expires_at'],
     );
   }
 }

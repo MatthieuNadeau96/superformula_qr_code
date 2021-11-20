344491f3d6798860be12144bc7dcc1c491649955
Matthieu <matthieunadeau96@gmail.com>
2021-11-20 10:43:37 -0500

    Refactor QrCodeScreen to avoid unnecessary rebuilds & add countdown timer stream

--- lib/screens/qr_code_screen.dart
+++ lib/screens/qr_code_screen.dart
@@ -1,10 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:superformula_qr_code/models/seed_model.dart';
 import 'package:superformula_qr_code/services/api_service.dart';
 
-class QrCodeScreen extends StatelessWidget {
+class QrCodeScreen extends StatefulWidget {
   const QrCodeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<QrCodeScreen> createState() => _QrCodeScreenState();
+}
+
+class _QrCodeScreenState extends State<QrCodeScreen> {
+  // Initialize api call & countdown stream outside of the build widget
+  final Future<SeedModel> _future =
+      ApiService().getSeed().timeout(const Duration(seconds: 15));
+  final _countdown = countdown();
 
   @override
   Widget build(BuildContext context) {
@@ -13,36 +26,63 @@ class QrCodeScreen extends StatelessWidget {
         title: const Text('QR Code'),
         centerTitle: true,
       ),
-      body: FutureBuilder<SeedModel>(
-        future: ApiService().getSeed(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (snapshot.hasData) {
-            var seed = snapshot.data!.seed;
-            print('seed -> $seed');
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FutureBuilder<SeedModel>(
+            future: _future,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (snapshot.hasData) {
+                var seed = snapshot.data!.seed;
+                // TODO If times up, disable qr code
+                return Center(
                   child: QrImage(
                     data: seed,
                     size: 250,
                   ),
-                ),
-                const SizedBox(height: 30),
-                const Text('15 s')
-              ],
-            );
-          }
-          return const Center(
-            child: Text('Something went wrong'),
-          );
-        },
+                );
+              }
+              return const Center(
+                child: Text('Something went wrong'),
+              );
+            },
+          ),
+          const SizedBox(height: 30),
+          StreamBuilder<int>(
+              stream: _countdown,
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  case ConnectionState.active:
+                    if (snapshot.hasData) {
+                      var count = snapshot.data;
+
+                      return Text('$count s');
+                    }
+                    break;
+                  // TODO Create restart button
+                  case ConnectionState.done:
+                    return const Text('Times up!');
+                  default:
+                }
+
+                return Container();
+              })
+        ],
       ),
     );
   }
 }
+
+// TODO Reverse timer so that it counts down from 15
+Stream<int> countdown() =>
+    Stream.periodic(const Duration(seconds: 1), (e) => e + 1).take(15);

fa2cbd3561ab9377da46d5e59412d30f0a3af343
Matthieu <matthieunadeau96@gmail.com>
2021-11-21 11:54:42 -0500

    refactor api_service_test with mockito

--- test/services/api_service_test.dart
+++ test/services/api_service_test.dart
@@ -1,15 +1,34 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:superformula_qr_code/models/seed_model.dart';
+import 'package:superformula_qr_code/services/api_service.dart';
 
-main() {
-  test("getSeed should return 200", () async {
-    // Arrange
-    var expected = 200;
-    // Act
-    final response =
-        await http.get(Uri.parse('http://192.168.1.242:8080/seed'));
-    var matcher = response.statusCode;
-    // Assert
-    expect(expected, matcher);
+import 'api_service_test.mocks.dart';
+
+// Generate a MockClient using the Mockito package
+@GenerateMocks([http.Client])
+void main() {
+  group('getSeed', () {
+    test('returns a SeedModel if the http call completes successfully',
+        () async {
+      final client = MockClient();
+      // return a successful response when we call the provided http.Client
+      when(client.get(Uri.parse('http://192.168.1.242:8080/seed'))).thenAnswer(
+          (_) async => http.Response('{"description": "seed generated"}', 200));
+
+      expect(await ApiService().getSeed(client), isA<SeedModel>());
+    });
+
+    test('throw an exception if the http call completes with an error',
+        () async {
+      final client = MockClient();
+      // return unsuccessful response when we call the provided http.Client
+      when(client.get(Uri.parse('http://192.168.1.242:8080/seed')))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(ApiService().getSeed(client), throwsException);
+    });
   });
 }
